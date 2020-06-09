52914d208a04a58c587a94b25786f680ac91dd7d
bslqy <853185345@qq.com>
2020-06-09 18:16:31 +1000

    二叉搜索树

--- src/Tree/BinarySortTreeDemo.java
+++ src/Tree/BinarySortTreeDemo.java
@@ -1,13 +1,28 @@
 package Tree;
 
 public class BinarySortTreeDemo {
+
+/**
+ *               7
+ *             /   \
+ *            3     10
+ *          /  \   /  \
+ *         1   5  9   12
+ *          \
+ *           2
+ */
     public static void main(String[] args) {
-        int[] arr = {7,3,10,12,5,1,9};
+        int[] arr = {7,3,10,12,5,1,9,2};
         BinaryTree2 binaryTree2 = new BinaryTree2();
         for (int i = 0; i < arr.length; i++) {
             binaryTree2.add(new Node(arr[i]));
         }
 
+        System.out.println("中序遍历二叉树");
+        binaryTree2.infixOrder(); //
+
+        binaryTree2.deleteNode(2);
+        System.out.println("删除节点后");
         binaryTree2.infixOrder(); //
     }
 
@@ -36,6 +51,55 @@ class BinaryTree2 {
 
     }
 
+    //查找要删除的节点
+    public Node search(int value) {
+        if (root == null) {
+            return null;
+        }
+        return root.search(value);
+    }
+
+    // 查找要删除节点的父节点
+    public Node searchParent(int value) {
+        if (root == null) {
+            return null;
+        }
+        return root.searchParent(value);
+    }
+
+    // 删除节点
+    public void deleteNode(int value) {
+        if (root == null) {
+            return;
+        }
+
+        // 1. 先找到要删除的节点
+        Node targetNode = search(value);
+        if (targetNode == null) {
+            return;
+        }
+        // 如果只有一个节点，则只需要全树删除
+        if (root.left == null && root.right == null) {
+            root = null;
+            return;
+        }
+
+        // 去找到TargetNode 的父节点
+        Node parent = searchParent(value);
+        // 情况1：如果删除的节点是叶子节点
+        if (targetNode.left == null && targetNode.right ==null) {
+            // 判断targetNode是parentNode的左或者右子节点
+            if (parent.left != null && parent.left.value == value) {
+                parent.left = null;
+            } else if (parent.right != null && parent.right.value == value) {
+                parent.right = null;
+            }
+
+        }
+
+
+    }
+
 
 }
 
@@ -43,6 +107,58 @@ class Node {
     int value;
     Node left;
     Node right;
+
+    // 查找要删除的节点
+
+    /***
+     *
+     * @param value 希望删除的节点的值
+     * @return 如果找到返回该节点，否则返回null
+     */
+    public Node search(int value) {
+        if (value == this.value) {
+            return this;
+        } else if (value < this.value ) {
+            if (this.left == null) {
+                return  null;
+            }
+            return this.left.search(value);
+        } else {
+            if (this.right == null) {
+                return null;
+            }
+            return this.right.search(value);
+        }
+    }
+
+    // 查找要删除节点的父节点
+
+    /***
+     *
+     * @param value 要找的节点的值
+     * @return
+     */
+
+    public Node searchParent(int value) {
+        if ((this.left != null && this.left.value == value) ||
+                (this.right != null && this.right.value == value)) {
+            return this;
+        }
+        else
+        {
+            // 如果查找的这个值小于当前节点的这个值，并且当前节点的左子节点不为空
+            if (value < this.value && this.left != null) {
+                return this.left.searchParent(value); // 递归向左查找
+            } else if (value >= value && this.right != null) {
+                return this.right.searchParent(value);// 递归向右查找
+            }
+            return null;
+        }
+
+
+    }
+
+
 
     public Node(int value) {
         this.value = value;
